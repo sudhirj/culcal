4f86140d79fe7f93b149f9563aed1839bb82d096
Sudhir Jonathan <sudhir.j@gmail.com>
2009-07-07 22:21:38 +0530

    cut margins and styled map better

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ DEBUG = True
 URL_VALIDATOR = '[^\w-]'
 MAP_KEY = 'ABQIAAAAdOCIY5iMV76jbHcybACMhxQi34lAQvM27VfTNvVaic9PtTFTExQT6O_D3Qz8PAwmNygHnmR1lYm7qQ'
 MAP_ENDPOINT = "http://maps.google.com/staticmap?sensor=false"
-MAP_SIZE = dict(x = 320, y = 300)
+MAP_SIZE = dict(x = 256, y = 256)
